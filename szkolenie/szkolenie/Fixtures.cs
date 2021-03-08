12ad3195289fb6e9f99fb0e100a0c9ed249c936e
Mateusz Orman <mateusz.orman@gmail.com>
2021-03-08 14:45:47 +0100

    LessonX added for my own tests

---
+++ szkolenie/szkolenie/Fixtures.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+
+namespace szkolenie
+{
+    [TestFixture]
+    public class Fixtures
+    {
+        ChromeDriver driver;
+        [TestFixtureSetUp]
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.seleniumeasy.com/test/basic-first-form-demo.html");
+            Thread.Sleep(1000);
+            driver.Manage().Window.Maximize();
+        }
+    }
+}
---
+++ szkolenie/szkolenie/LessonX.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+
+namespace szkolenie
+{
+    class LessonX
+    {
+        [TestFixtureSetUp][Test]
+        public void CheckBox()
+        {
+            
+        }
+    }
+}
