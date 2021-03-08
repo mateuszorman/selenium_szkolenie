858939cb38a2255490a484fd90dbb6593cd4adbe
Mateusz Orman <mateusz.orman@gmail.com>
2021-03-08 10:13:37 +0100

    Lesson_1

---
+++ szkolenie/szkolenie/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("szkolenie")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("szkolenie")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fe4a6e50-b751-487a-b4a4-fe83b54b1a20")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ szkolenie/szkolenie/UnitTest1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Szkolenie
+{
+    public class Hello
+    {
+        ChromeDriver driver;
+
+        [Test]
+        public void CheckInput()
+        {
+            string typed_text = "AAAAAAAAA!!!";
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.seleniumeasy.com/test/basic-first-form-demo.html");
+            Thread.Sleep(1000);
+            driver.Manage().Window.Maximize();
+            driver.FindElement(By.ClassName("at-cm-no-button")).Click();
+            driver.FindElement(By.Id("user-message")).SendKeys(typed_text);
+            driver.FindElements(By.ClassName("btn-default"))[0].Click();
+            string read_text = driver.FindElement(By.Id("display")).Text;
+            Assert.AreEqual(read_text, typed_text);
+            driver.Quit();
+        }
+
+        [Test]
+        public void CheckSum()
+        {
+            int number_1 = 4;
+            int number_2 = 3;
+            int result = 7;
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.seleniumeasy.com/test/basic-first-form-demo.html");
+            Thread.Sleep(1000);
+            driver.Manage().Window.Maximize();
+            driver.FindElement(By.ClassName("at-cm-no-button")).Click();
+            driver.FindElement(By.Id("sum1")).SendKeys(number_1.ToString());
+            driver.FindElement(By.Id("sum2")).SendKeys(number_2.ToString());
+            driver.FindElements(By.ClassName("btn-default"))[1].Click();
+            string read_text = driver.FindElement(By.Id("displayvalue")).Text;
+            Assert.AreEqual(Convert.ToInt32(read_text), result);
+            driver.Quit();
+        }
+    }
+}
