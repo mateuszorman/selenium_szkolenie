2b39db26e4103aec73b20a3b4adce9cf93846478
Mateusz Orman <mateusz.orman@gmail.com>
2021-03-22 12:21:31 +0100

    Refactor - SetUp, comments added

--- szkolenie/szkolenie/Lesson2.cs
+++ szkolenie/szkolenie/Lesson2.cs
@@ -9,70 +9,61 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 
-namespace Szkolenie
+namespace szkolenie
 {
-    public class Lesson2
+    public class Lesson2 : Fixtures
     {
-        ChromeDriver driver;
-        [Test]
-        public void CheckBox()
+        [TestCase("https://www.seleniumeasy.com/test/basic-checkbox-demo.html", "false")]
+        public void CheckBox(string URL, string startup_popup)
         {
-            driver = new ChromeDriver();
-            driver.Manage().Window.Maximize();
-            driver.Navigate().GoToUrl("https://www.seleniumeasy.com/test/basic-checkbox-demo.html");
-            Thread.Sleep(1000);
-
+            // arrange
             var logoText = driver.FindElement(By.LinkText("Selenium Easy"));
             var option = driver.FindElement(By.CssSelector("ul+ .checkbox label"));
             var singleCheckBoxBTN = driver.FindElement(By.XPath("/html/body/div[2]/div/div[2]/div[1]/div[2]/div[1]/label"));
+            var windowHandlesBefore = driver.WindowHandles;
 
+            // act
+            Assert.AreEqual("Selenium Easy - Checkbox demo for automation using selenium", driver.Title);
             singleCheckBoxBTN.Click();
             option.Click();
             logoText.Click();
+            var windowHandlesAfter = driver.WindowHandles;
+            driver.SwitchTo().Window(driver.WindowHandles.Last());
 
-            driver.Quit();
+            // assert
+            Assert.AreEqual("Learn Selenium with Best Practices and Examples | Selenium Easy", driver.Title);            
         }
 
-        [Test]
-        public void Table()
+        [TestCase("https://www.seleniumeasy.com/test/table-pagination-demo.html", "false")]
+        public void Table(string URL, string startup_popup)
         {
-            driver = new ChromeDriver();
-            driver.Manage().Window.Maximize();
-            driver.Navigate().GoToUrl("https://www.seleniumeasy.com/test/table-pagination-demo.html");
-            Thread.Sleep(1000);
-
+            // arrange
             var table = driver.FindElement(By.ClassName("table-responsive"));
             var rows = table.FindElements(By.TagName("tr"));
             var cellFirstRaw = rows[1].FindElements(By.TagName("td"));
+
+            // act
             var secondCell = cellFirstRaw[1];
 
+            // assert
             Assert.AreEqual("Table cell", secondCell.Text);
-
-            driver.Quit();
         }
 
-        [Test]
-        public void DropDown()
+        [TestCase("https://www.seleniumeasy.com/test/basic-select-dropdown-demo.html", "false")]
+        public void DropDown(string URL, string startup_popup)
         {
-            driver = new ChromeDriver();
-            driver.Manage().Window.Maximize();
-            driver.Navigate().GoToUrl("https://www.seleniumeasy.com/test/basic-select-dropdown-demo.html");
-            Thread.Sleep(1000);
-
+            // arrange
             var selectADay = new SelectElement(driver.FindElement(By.Id("select-demo")));
             var selectedValue = driver.FindElement(By.ClassName("selected-value"));
 
+            // act
             selectADay.SelectByText("Monday");
             selectADay.SelectByIndex(5);
             selectADay.SelectByValue("Sunday");
-
             string selectedValueEdited = selectedValue.Text.Substring(16);
 
+            // assert
             Assert.AreEqual("Sunday", selectedValueEdited);
-
-            Thread.Sleep(1000);
-
-            driver.Quit();
         }
     }
 }
